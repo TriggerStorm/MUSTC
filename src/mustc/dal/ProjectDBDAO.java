ca678c37e78a6f076ed89760052466730ab99fbe
SnowBoy73 <alan4999@gmail.com>
2020-04-22 16:08:14 +0200

    ProjectDBDAO - editProject

--- src/mustc/dal/ProjectDBDAO.java
+++ src/mustc/dal/ProjectDBDAO.java
@@ -34,8 +34,8 @@ public class ProjectDBDAO {
     
      public Project addNewProjectToDB(String name, Client associatedClient, float projectRate, boolean closed) throws SQLException { 
     //  Adds a new project to the DB, and returns the updated projectList to the GUI
-        String sql = "INSERT INTO PROJECTS(name, associatedClient, projectRate, closed) VALUES (?,?,?,?)";
         String clientName = associatedClient.getName();
+        String sql = "INSERT INTO PROJECTS(name, clientName, projectRate, closed) VALUES (?,?,?,?)";
         List<Task> emptyTaskList = new ArrayList<>();
         emptyTaskList = null;
         Project newProject = new Project(0, name, associatedClient, projectRate, emptyTaskList, closed);
@@ -45,6 +45,10 @@ public class ProjectDBDAO {
             pstmt.setString(2, clientName);
             pstmt.setFloat(3, projectRate);
             pstmt.setBoolean(4, closed);
+/*            int isClosed = 0;  // can we put boolean in the database?
+            if(closed == true)
+                isClosed = 1;
+ */   
             int affectedRows = pstmt.executeUpdate();
             if (affectedRows == 0) {
                 throw new SQLException("Creating attendance failed, no rows affected.");
@@ -61,7 +65,40 @@ public class ProjectDBDAO {
         } catch (SQLException ex) {
             Logger.getLogger(ProjectDBDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         return newProject;
     }
+     
+     
+    public Project editProject (Project editedProject, String name, Client associatedClient, float projectRate, boolean closed) { 
+    //  Edits a Project in the Projects table of the database given the Projects new details.  
+        String clientName = associatedClient.getName();
+        String sql = "UPDATE Projects SET name = clientName, projectRate = ?, closed = ? WHERE email = ?";
+        try ( Connection con = dbc.getConnection()) {
+            //Create a prepared statement.
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            //Set parameter values.
+            pstmt.setString(1, name);
+            pstmt.setString(2, clientName);
+            pstmt.setFloat(3, projectRate);
+            pstmt.setBoolean(4, closed);
+/*            int isClosed = 0;  // can we put boolean in the database?
+            if(closed == true)
+                isClosed = 1;
+ */          
+            //Execute SQL query.
+            pstmt.executeUpdate();
+            editedProject.setName(name);
+            editedProject.setAssociatedClient(associatedClient);  
+            editedProject.setProjectRate(projectRate);
+            editedProject.setClosed(closed);
+            return editedProject;
+        } catch (SQLServerException ex) {
+            Logger.getLogger(ProjectDBDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProjectDBDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+      
+      
 }
