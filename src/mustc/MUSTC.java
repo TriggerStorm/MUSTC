6f8516f6b7a4b7e96adabe11426de272f310b5ea
TriggerStorm <michael.h.p@hotmail.com>
2020-05-05 14:01:59 +0200

    Update MUSTC.java
    al

--- src/mustc/MUSTC.java
+++ src/mustc/MUSTC.java
@@ -21,7 +21,7 @@ public class MUSTC extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("gui/view/UserView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/Test.fxml"));
 
         
         Scene scene = new Scene(root);
